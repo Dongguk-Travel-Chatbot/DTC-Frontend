7005d1e8403db6d5e37766fd5facdfede22b0c1c
jouijae <kuda1390@dgu.ac.kr>
2024-08-26 17:30:41 +0900

    feat: community page create

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,16 +3,43 @@ import "./style.css";
 import Like from "src/assets/Like.svg"
 import Comment from "src/assets/Comment.svg"
 import Scrap from "src/assets/Scrap.svg"
+import ProfileDog from "src/assets/ProfileDog.svg";
+import ProfileRabbit from "src/assets/ProfileRabbit.svg";
+import ProfileBear from "src/assets/ProfileBear.svg";
+import ProfileFox from "src/assets/ProfileFox.svg";
+import ProfileCat from "src/assets/ProfileCat.svg";
+import ProfileKoala from "src/assets/ProfileKoala.svg";
+import ProfileLion from "src/assets/ProfileLion.svg";
+
 
 export const Post = ({ className, props }) => {
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const month = date.getMonth()+1;
+    const day = date.getDate();
+    return `${month}/${day}`;
+  }
+
+  const profileImages = {
+    1: ProfileDog,
+    2: ProfileRabbit,
+    3: ProfileBear,
+    4: ProfileFox,
+    5: ProfileCat,
+    6: ProfileKoala,
+    7: ProfileLion,
+  };
+  
+  const profileImage = profileImages[props.user.image];
+
   return (
     <div className={`post ${className}`}>
       <div className="div">{props.title}</div>
       
       <div className="text-wrapper-2">{props.user.nickname}</div>
-      <img className="image" alt="Image" src="https://c.animaapp.com/lk34aNoN/img/image-99-1@2x.png" />
+      <img className="image" alt="Image" src={profileImage} />
 
-      
       <div className="post-like">
         <img className="vector" alt="Vector" src={Like} />
         <div className="text-wrapper-4">{props.like}</div>
@@ -29,7 +56,9 @@ export const Post = ({ className, props }) => {
         <div className="text-wrapper-3">{props.scrap}</div>
       </div>
 
-      <div className="text-wrapper">6/14</div>
+      <div className="text-wrapper">
+        {props.post_time ? formatDate(props.post_time) : 'dateError'}
+      </div>
 
       <img className="line" alt="Line" src="https://c.animaapp.com/lk34aNoN/img/line-5-1@2x.png" />
     </div>
