c377002d8f06178b156eb59ae83a42fff1ee97e0
jouijae <kuda1390@dgu.ac.kr>
2024-08-27 15:11:25 +0900

    feat: mypage create

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -4,20 +4,21 @@ import { Redirection } from "../pages/Redirection/Redirection";
 import { SignUp } from "../pages/SignUp";
 import { TravelCommunty } from "src/pages/TravelCommunty";
 import { Main } from "src/pages/Main";
+
 import { Mypage } from "src/pages/Mypage";
 
 export const AppRouter = () => {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route exact path='/kakao/callback' element={<Redirection />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/travelcommunity' element={<TravelCommunty />} />
-          <Route path='/main' element={<Main />} />
-          <Route path='/mypage' element={<Mypage />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  };
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route exact path='/kakao/callback' element={<Redirection />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/travelcommunity' element={<TravelCommunty />} />
+        <Route path='/main' element={<Main />} />
+        
+        <Route path='/mypage' element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
