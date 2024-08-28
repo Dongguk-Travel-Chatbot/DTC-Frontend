965a2209c091b64c0616d4bdcdd1f06c5c8d30c0
Jang99u <alswkdrb@dgu.ac.kr>
2024-08-28 16:13:50 +0900

    feat: BigOutline 컴포넌트 수정

--- src/components/BigOutline/BigOutline.jsx
+++ src/components/BigOutline/BigOutline.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./style.css";
 
-export const BigOutline = ({ className, label, setInputValue }) => {
+export const BigOutline = ({ className, label, setInputValue, initialValue }) => {
   return (
     <div className={`big-outline ${className}`}>
       <div className="input-label">
-        <input className="text-wrapper" onChange={(e)=>{ setInputValue(e.target.value) }}></input>
+        <input
+          className="text-wrapper"
+          defaultValue={initialValue} // 기본값 설정
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        />
       </div>
       <div className="label">{label}</div>
     </div>
