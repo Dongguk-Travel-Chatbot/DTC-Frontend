284f4a1fb3e69a8530c479631c3caf2c9490b5fa
jouijae <kuda1390@dgu.ac.kr>
2024-08-31 18:07:56 +0900

    feat: 상세조회 오류 수정 및 카카오맵 초안

--- src/components/CommentMono/CommentMono.jsx
+++ src/components/CommentMono/CommentMono.jsx
@@ -1,8 +1,4 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
+import React, { useState } from "react";
 import ProfileDogCircle from "src/assets/ProfileDogCircle.svg";
 import ProfileRabbitCircle from "src/assets/ProfileRabbitCircle.svg";
 import ProfileBearCircle from "src/assets/ProfileBearCircle.svg";
@@ -10,13 +6,14 @@ import ProfileFoxCircle from "src/assets/ProfileFoxCircle.svg";
 import ProfileCatCircle from "src/assets/ProfileCatCircle.svg";
 import ProfileKoalaCircle from "src/assets/ProfileKoalaCircle.svg";
 import ProfileLionCircle from "src/assets/ProfileLionCircle.svg";
-import React from "react";
 import { ReComment } from "../ReComment";
 import "./style.css";
 
-export const CommentMono = ({ className , props, commentMono}) => {
-  const temp = commentMono
-  console.log(temp)
+export const CommentMono = ({ className, props, commentMono }) => {
+  const [comment, setComment] = useState(""); // 입력값을 저장하는 상태
+
+  const temp = commentMono;
+  console.log(temp);
   const profiles = [
     ProfileDogCircle,
     ProfileRabbitCircle,
@@ -26,35 +23,52 @@ export const CommentMono = ({ className , props, commentMono}) => {
     ProfileKoalaCircle,
     ProfileLionCircle,
   ];
+  
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const month = date.getMonth()+1;
+    const month = date.getMonth() + 1;
     const day = date.getDate();
-    
+
     return `${month}/${day}`;
-  }
+  };
 
   const formatDate2 = (dateString) => {
     const date = new Date(dateString);
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-  
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
     return `${hours}:${minutes}`;
-  }
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value); // 입력된 값을 상태에 저장
+  };
+
+  const handleSubmit = () => {
+    // 댓글 제출 로직 처리
+    console.log("Submitted comment:", comment);
+    setComment(""); // 입력창 초기화
+  };
 
   return (
     <div className={`comment-mono ${className}`}>
       <div className="comment-content">
-        <div className="text-wrapper-4">
-          {commentMono[0].content}
-        </div>
+        <div className="text-wrapper-4">{commentMono[0].content || null}</div>
       </div>
-      
-      <img className="comment-writer-icons" alt="image" src={profiles[commentMono[0].user.image]}/>
-      <div className ="commment-total">
+
+      <img
+        className="comment-writer-icons"
+        alt="image"
+        src={profiles[commentMono[0].user.image] || null}
+      />
+      <div className="comment-total">
         <div className="comment-info">
-          <img className="like-icon" alt="Like icon" src="https://c.animaapp.com/CeWmk5Rx/img/like-icon-5@2x.png" />
-          <div className="text-wrapper-5">{props.like}</div>
+          <img
+            className="like-icon"
+            alt="Like icon"
+            src="https://c.animaapp.com/CeWmk5Rx/img/like-icon-5@2x.png"
+          />
+          <div className="text-wrapper-5">{commentMono[0].like || 0}</div>
           <div className="text-wrapper-6">{formatDate(props.postTime)}</div>
           <div className="text-wrapper-7">{formatDate2(props.postTime)}</div>
           <div className="re-comment-button">
@@ -64,9 +78,15 @@ export const CommentMono = ({ className , props, commentMono}) => {
           </div>
         </div>
 
-        <div className="comment-writer" src ="https://c.animaapp.com/CeWmk5Rx/img/image-98@2x.png">
-          <div className="text-wrapper-9">{commentMono[0].user.nickname}</div>
-          <img className="edit-icons" alt="Edit icons" src="https://c.animaapp.com/CeWmk5Rx/img/editicons-3@2x.png" />
+        <div className="comment-writer">
+          <div className="text-wrapper-9">
+            {commentMono[0].user.nickname || null}
+          </div>
+          <img
+            className="edit-icons"
+            alt="Edit icons"
+            src="https://c.animaapp.com/CeWmk5Rx/img/editicons-3@2x.png"
+          />
           <div className="modify-button">
             <div className="div-wrapper">
               <div className="text-wrapper-10">수정하기</div>
@@ -79,22 +99,31 @@ export const CommentMono = ({ className , props, commentMono}) => {
           </div>
         </div>
 
-        {commentMono.filter((_, index) => index > 0)
-        .map((reComment, index) => (
-          <ReComment key={index} className="re-comment-instance" commentMono={reComment}/>
-        ))}
+        {commentMono
+          .filter((_, index) => index > 0)
+          .map((reComment, index) => (
+            <ReComment
+              key={index}
+              className="re-comment-instance"
+              commentMono={reComment}
+            />
+          ))}
         <div className="re-post-comment">
-          <div className="post-comment-box">
-            <div className="text-wrapper-18">입력하세요!!</div>
-          </div>
+          <input
+            type="text"
+            className="post-comment-box"
+            placeholder="입력하세요"
+            value={comment} // 상태값을 input의 value에 바인딩
+            onChange={handleCommentChange} // 값이 변경될 때마다 handleCommentChange 호출
+          />
           <img
             className="post-comment-button"
             alt="Post comment button"
             src="https://c.animaapp.com/CeWmk5Rx/img/postcommentbutton-2@2x.png"
+            onClick={handleSubmit} // 버튼 클릭 시 handleSubmit 호출
           />
-          <img className="post-comment-writer" src = {profiles[0]}/>  {/*수정해야할 부분*/}
+          <img className="post-comment-writer" src={profiles[0]} />{" "}
         </div>
-
       </div>
     </div>
   );
