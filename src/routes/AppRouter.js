ac0426cbb2bd595ac6ed5daf08540fff5b180d62
thisminji <thisminji@dgu.ac.kr>
2024-08-31 18:51:01 +0900

    feat: 화면전환 애니메이션 구현

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { LoginPage } from "../pages/Login/LoginPage";
 import { Redirection } from "../pages/Redirection/Redirection";
-import { SignUp } from "../pages/SignUp";
+import { SignUp } from "src/pages/SignUp";
 import { TravelCommunity } from "src/pages/TravelCommunity";
 import { Main } from "src/pages/Main";
 import { Community } from "src/pages/Community";
@@ -22,36 +23,63 @@ import { Travelmakecallender2 } from "src/pages/Travelmakecallender2/Travelmakec
 import { Travelmakecallender3 } from "src/pages/Travelmakecallender3/Travelmakecallender3";
 import { TravelMakeMember } from "src/pages/TravelMakeMember";
 import { TravelMakeFInal } from "src/pages/TravelMakeFInal";
+import { AnimatePresence, motion } from "framer-motion";
 
-export const AppRouter = () => {
+const AnimatedRoutes = () => {
+    const location = useLocation();
 
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route exact path='/kakao/callback' element={<Redirection />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/travelCommunity' element={<TravelCommunity />} />
-          <Route path='/main' element={<Main />} />
-          <Route path='/community' element={<Community />} />
-          <Route path='/mypage' element={<Mypage />} />
-          <Route path='/mypage-edit' element={<MypageEdit />} />
-          <Route path='/travel-record' element={<TravelRecordPage />} />
-          <Route path='/travel-plan' element={<TravelPlanPage />} />
-          <Route path='/travel-post' element={<TravelPost />} />
-          <Route path='/TravelClip' element={<TravelClip />} />
-          <Route path='/freeCommunity' element={<FreeCommunity />} />
-          <Route path='/hotCommunity' element={<HotCommunity />} />
-          <Route path='/free-post' element={<FreePost />} />
-          <Route path="/post-details/:category/:id" element={<PostDetails />} />
-          <Route path="/travel-make-basic" element={<TravelMakeBasic />} />
-          <Route path="/travel-callender1" element={<Travelmakecallender1 />} />
-          <Route path="/travel-callender2" element={<Travelmakecallender2 />} />
-          <Route path="/travel-callender3" element={<Travelmakecallender3 />} />
-          <Route path="/travel-make-member" element={<TravelMakeMember />} />
-          <Route path="/travel-make-final" element={<TravelMakeFInal />} />
-          <Route path="/100" element={<ChatAiTravel />} />
-        </Routes>
-      </BrowserRouter>
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                
+                {/* 애니메이션 미적용 경로 */}
+                <Route path="/" element={<LoginPage />} />
+                <Route exact path='/kakao/callback' element={<Redirection />} />
+                <Route path='/sign-up' element={<SignUp />} />
+                <Route path='/main' element={<Main />} />
+                <Route path='/mypage' element={<Mypage />} />
+                <Route path='/mypage-edit' element={<MypageEdit />} />
+                <Route path='/travel-record' element={<TravelRecordPage />} />
+                <Route path='/travel-plan' element={<TravelPlanPage />} />
+                <Route path='/travel-post' element={<TravelPost />} />
+                <Route path='/TravelClip' element={<TravelClip />} />
+                <Route path='/free-post' element={<FreePost />} />
+                <Route path='/community' element={<Community />} />
+                <Route path="/post-details/:category/:id" element={<PostDetails />} />
+                <Route path="/travel-callender2" element={<Travelmakecallender2 />} />
+                <Route path="/travel-callender3" element={<Travelmakecallender3 />} />
+
+                {/* 애니메이션 적용 경로 */}
+                <Route path='/freeCommunity' element={<PageTransition><FreeCommunity /></PageTransition>} />
+                <Route path='/hotCommunity' element={<PageTransition><HotCommunity /></PageTransition>} />
+                <Route path='/travelCommunity' element={<PageTransition><TravelCommunity /></PageTransition>} />
+                <Route path="/travel-make-basic" element={<PageTransition><TravelMakeBasic /></PageTransition>} />
+                <Route path="/travel-callender1" element={<PageTransition><Travelmakecallender1 /></PageTransition>} />  
+                <Route path="/travel-make-member" element={<PageTransition><TravelMakeMember /></PageTransition>} />
+                <Route path="/travel-make-final" element={<PageTransition><TravelMakeFInal /></PageTransition>} />
+                <Route path="/100" element={<PageTransition><ChatAiTravel /></PageTransition>} />
+            </Routes>
+        </AnimatePresence>
     );
-  };
+};
+
+// 페이지 전환 시 슬라이드 애니메이션을 적용하는 컴포넌트
+const PageTransition = ({ children }) => (
+    <motion.div
+        initial={{ x: "100%" }} // 새 페이지가 오른쪽에서 시작
+        animate={{ x: 0 }} // 제자리로 슬라이드
+        exit={{ x: "-100%" }} // 이전 페이지는 왼쪽으로 슬라이드 아웃
+        transition={{ duration: 0.2 }} // 애니메이션 지속 시간
+        style={{ position: "absolute", width: "100%" }} // 페이지를 겹치게 배치
+    >
+        {children}
+    </motion.div>
+);
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <AnimatedRoutes />
+        </BrowserRouter>
+    );
+};
